9e088fa01daf5d9e939c78faf23652367fe6b60c
ibrahim-khalil <ibrahimkhalil@users.noreply.github.com>
2020-10-08 02:36:34 +0500

    commit

---
+++ precache-manifest.2ea75029a8c1700141ef0b91def626de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e2218eebb8ab4090f709e401264503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "785513a9205286641f4b",
+    "url": "/static/css/main.5d144345.chunk.css"
+  },
+  {
+    "revision": "896cc49df2e08f5d1d20",
+    "url": "/static/js/2.e4b962f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.e4b962f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "785513a9205286641f4b",
+    "url": "/static/js/main.d9541042.chunk.js"
+  },
+  {
+    "revision": "466c1a7fdaa58576fa1f",
+    "url": "/static/js/runtime-main.7d4b7ba4.js"
+  },
+  {
+    "revision": "fa275866a285661bf4cc157bad24a6fb",
+    "url": "/static/media/tom-cruise.fa275866.jpg"
+  }
+]);
