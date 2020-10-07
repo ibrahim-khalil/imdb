56a8c0a1af0c135712d87d8b921382c36da4f423
ibrahim-khalil <ibrahimkhalil@users.noreply.github.com>
2020-10-08 02:41:06 +0500

    commit

---
+++ precache-manifest.fd91cc0a258ddd935a5c908b21582451.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76250bc0c8922b37e6b3d3b24cfcc0a",
+    "url": "/imdb/index.html"
+  },
+  {
+    "revision": "9692477e3aa6e0a22158",
+    "url": "/imdb/static/css/main.5d144345.chunk.css"
+  },
+  {
+    "revision": "896cc49df2e08f5d1d20",
+    "url": "/imdb/static/js/2.e4b962f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/imdb/static/js/2.e4b962f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9692477e3aa6e0a22158",
+    "url": "/imdb/static/js/main.8c19e37d.chunk.js"
+  },
+  {
+    "revision": "cefd6c43a54e6d1f38fc",
+    "url": "/imdb/static/js/runtime-main.056ef7cc.js"
+  },
+  {
+    "revision": "fa275866a285661bf4cc157bad24a6fb",
+    "url": "/imdb/static/media/tom-cruise.fa275866.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ea75029a8c1700141ef0b91def626de.js"
+  "/imdb/precache-manifest.fd91cc0a258ddd935a5c908b21582451.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/imdb/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
